7e5c26a82797e19f427da26f419f0d82b6d87c2d
Omesh01 <lakshithaomesh123@gmail.com>
2024-06-15 02:51:11 +0530

    Initial commit

---
+++ Omesh/app/src/main/java/com/example/Omesh/Addjob.kt
@@ -0,0 +1,11 @@
+package com.example.gayasha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Addjob : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addjob)
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MAD-MarketPlace
+ A mobile application for an online marketplace was part of the project. We developed in Kotlin,
